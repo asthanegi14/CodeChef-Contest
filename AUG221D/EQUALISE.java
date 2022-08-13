884d27e848c851878dcf49e181b58339645994f2
Astha Negi <asthanegi539@gmail.com>
2022-08-13 20:13:06 +0530

    Create EQUALISE.java

---
+++ AUG221D/EQUALISE.java
@@ -0,0 +1,39 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0){
+    		    
+    		int a = sc.nextInt();
+    		int b = sc.nextInt();
+    	
+    		if(a>b){
+    		    while(b<a){
+    		        b *= 2; //keep increasing b until it becomes greater tha or equals to a
+    		    }
+    		}
+    		else{
+    		    while(a<b){
+    		        a *= 2; //keep increasing a until it becomes greater tha or equals to b
+    		    }
+    		}
+    		if(a==b){
+    		    System.out.println("YES");
+    		}
+    		else{
+    		    System.out.println("NO");
+    		}
+		    t--;
+		}
+	}
+}
