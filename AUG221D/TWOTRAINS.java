3e084bc0ed608ebec4f94da93e933535a6a70137
Astha Negi <asthanegi539@gmail.com>
2022-08-13 20:25:37 +0530

    Update TWOTRAINS.java

--- AUG221D/TWOTRAINS.java
+++ AUG221D/TWOTRAINS.java
@@ -1,15 +1,11 @@
-/* package codechef; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
 		
