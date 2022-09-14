882d5345a554d679e55ecd157a1ebb1fbec94de1
Astha Negi <asthanegi539@gmail.com>
2022-09-14 23:38:15 +0530

    Create  CHEFGAMES.java

---
+++ START56D/CHEFGAMES.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    int c = sc.nextInt();
+		    int d = sc.nextInt();
+		    
+		    if(a==1||b==1||c==1||d==1){
+		        System.out.println("OUT");
+		    }
+		    else{
+		        
+		        System.out.println("IN");
+		    }
+		}
+	}
+}
