bcc6c9b43abc1b1e0b808f1dcf36089302ba45fa
Astha Negi <asthanegi539@gmail.com>
2022-09-22 17:07:57 +0530

    Create  TFPAPER.java

---
+++ LTIME110D/TFPAPER.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		   
+		 
+		     System.out.println(a-b);
+		  
+		    t--;
+		}
+	}
+}
