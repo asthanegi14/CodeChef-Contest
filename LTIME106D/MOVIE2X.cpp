1ddec6739784a0bcb09aab6337fc3efbe4153a50
Astha Negi <asthanegi539@gmail.com>
2022-09-22 17:00:41 +0530

    Create MOVIE2X.cpp

---
+++ LTIME106D/MOVIE2X.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x, y;
+	cin>>x>>y;
+	
+    float new_time;
+	new_time=x-(y/2);
+	
+	cout<<new_time;
+
+	return 0;
+}
