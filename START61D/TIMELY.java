f244d5a3e395bf537a2fcc3ac915da5bace1bb3d
Astha Negi <asthanegi539@gmail.com>
2022-11-24 18:12:52 +0530

    Create  TIMELY.java

---
+++ START61D/TIMELY.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t=sc.nextInt();
+		
+		while(t-->0){
+		    int x = sc.nextInt();
+		    if(x>=30){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
